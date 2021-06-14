98ecfdbd3b3386c87813ddb5861d808c7bc6e33e
Pourya Bagheri <cepsociety@gmail.com>
2021-06-14 10:15:35 +0300

    Update SafeMath.sol

--- SafeMath.sol
+++ SafeMath.sol
@@ -46,3 +46,29 @@ library SafeMath {
 
         return c;
     }
+
+
+ /**
+     * @dev Returns the multiplication of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `*` operator.
+     *
+     * Requirements:
+     * - Multiplication cannot overflow.
+     */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+
+        return c;
+    }
+    
+    
